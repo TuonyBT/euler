23ce9389938aa0033410546cfdd35399c1ed2e2e
tuonybt <tonybt.tbt@gmail.com>
2023-05-08 14:18:56 +0100

    Problem 3 solved

--- src/euler/problemss_100.rs
+++ src/euler/problemss_100.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeMap};
+
 // Problem 1: sum of numbers less than n which are multiples of 3 or 5
 
 pub fn multiples_of3and5(n: i32) -> i32 {
@@ -54,3 +56,60 @@ pub fn fibo_index(n: i32) -> i32 {
 
 }
 
+// Problem 3: largest prime factor
+
+pub fn largest_prime_factor(n: usize) -> usize {
+    let mut prime_factors = BTreeMap::<usize, usize>::new();
+    let mut m = n;
+
+
+//  Try reducing the size of the primes vector we need to test by pulling out first few primes manually
+    for seed in [2, 3, 5].into_iter() {
+        while m / seed * seed == m {
+            m /= seed;
+            *prime_factors.entry(seed).or_insert(0) += 1; 
+        }
+    }
+
+
+//  if the cofactor of the seeds we have tested is less than the square of the next prime, the slice indexation will not work...
+    if m > 48 {
+        let lim = (m as f64).powf(0.5) as usize + 1;
+        for p in &sieve_of_eratothsenes(lim)[5..] {
+            let seed = *p as usize;
+            while m / seed * seed == m {
+                m /= seed;
+                *prime_factors.entry(seed).or_insert(0) += 1; 
+            }
+        }
+    }
+
+//  in which case the cofactor itself must be prime
+    else if m > 1 {
+        prime_factors.insert(m, 1);         
+    }
+
+    let hpf = match prime_factors.keys().last() {
+        Some(z) => *z,
+        None => 1,
+    };
+    hpf
+}
+
+pub fn sieve_of_eratothsenes(x: usize) -> Vec<usize> {
+    let mut sieve = vec![true; x + 1];
+    let mut lp: usize = 2;
+    while lp <= (x as f64).sqrt().floor() as usize {
+        let fnp = lp.pow(2);
+        for idx in (fnp..sieve.len()).step_by(lp) {
+            sieve[idx] = false;
+        }
+        lp = match sieve[lp + 1..].iter().position(|z| z == &true) {
+            Some(y) => y + lp + 1,
+            None => x,
+        };
+    }
+    let primes = sieve.iter().enumerate().filter(|z| z.1 == &true).map(|z| z.0).collect::<Vec<usize>>();
+    primes
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ pub mod euler;
 pub use crate::euler::problemss_100::*;
 
 fn main() {
-    for n in 0..12 {
-//        println!("{} {} {}", n, binet(n), fibo_even_sum(binet(n)));
+    for n in 1..50 {
+        println!("{} {}", n, largest_prime_factor(n));
     }
 }
 
@@ -31,6 +31,16 @@ mod tests {
         assert_eq!(fibo_even_sum(1000), 798);
         assert_eq!(fibo_even_sum(100000), 60696);
         assert_eq!(fibo_even_sum(4000000), 4613732);
+    }
+    #[test]
+    fn problem_3() {
+        assert_eq!(largest_prime_factor(2), 2);
+        assert_eq!(largest_prime_factor(3), 3);
+        assert_eq!(largest_prime_factor(5), 5);
+        assert_eq!(largest_prime_factor(7), 7);
+        assert_eq!(largest_prime_factor(8), 2);
+        assert_eq!(largest_prime_factor(13195), 29);
+        assert_eq!(largest_prime_factor(600851475143), 6857);
     }
 }
 
